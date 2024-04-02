49427b4bc7a157e33f41a14095cc21c24e788c7a
azamatjon-abdullayev <azamatjonabdullayev485@gmail.com>
2024-04-02 21:36:07 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,88 @@
+// "use script";
+// // number
+// let number = 11;
+// console.log(-8 / 0); // infinity
+// console.log('azamat' * 8); // NaN
+
+// // string
+
+// let name = 'azamat';
+
+// // boolean
+
+// let isMarried = false;
+// let isCircleEarth = true;
+
+// // null
+
+// console.log(age);
+
+// // undefined
+
+// let nomi;
+//////////////
+// console.log(nomi);
+
+// const object = {
+//     age: 10,
+//     ismi: 'ss'
+// };
+
+// console.log(object.ismi);
+
+////////////////////////////////////////////////////
+
+
+// const place = confirm('saytdamisiz');
+// console.log(place)
+
+
+// const checkYosh = +prompt('yoshingiz nechchida','16')
+// console.log(typeof checkYosh);
+
+////////////////////////////////////////////////////
+
+// let baza = ['']
+
+// baza[0] = prompt('ismingizni kiriting')
+// baza[1] = prompt('familyangizni kiriting')
+// baza[2] = +prompt('yoshingizni kiriting')
+
+// console.log(baza);
+
+//////////////////////////////////////////
+
+// const channel = prompt("What's your favourite youtuber");
+
+// console.log(`https://youtube.com/${channel}/videos`);
+
+// const age = '16';
+// const name = 'azamat';
+
+// console.log(`salom mening ismim ${name}, va men ${age} yoshdaman`);
+
+////////////////////////////////////////////////
+
+// let son = 10;
+// let son2 = 10;
+
+// console.log(son++);
+// console.log(son2--);
+
+// console.log(son)
+// console.log(son2)
+
+//////////////////////////////////////////////////////
+
+// const isAge = true;
+// const isClose = false;
+// const iii = true;
+// const ii1i = true;
+// const iii4 = true;
+// const iii5= true;
+// const ii2i = true;
+
+// console.log(isAge && !isClose && iii);
+
+
+console.log(5*5==!'10');
